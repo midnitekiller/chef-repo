3fc4dccfeaa1b1dfb569e1203e000e4c11a0608d
4lt4i <github@altai.se>
2014-02-04 10:42:03 -0800

    Updated chromedriver.

--- cookbooks/watir/attributes/default.rb
+++ cookbooks/watir/attributes/default.rb
@@ -1,3 +1,3 @@
 default['watir::ie']['http_url'] = "https://selenium.googlecode.com/files/IEDriverServer_x64_2.35.3.zip"
-default['watir::chrome']['http_url'] = "http://chromedriver.storage.googleapis.com/2.4/chromedriver_win32.zip"
+default['watir::chrome']['http_url'] = "http://chromedriver.storage.googleapis.com/2.9/chromedriver_win32.zip"
 default['watir::setup']['path'] = "C:\\watir"
